d5b7431a32764138cbbf733f96d3519ba1b630ef
nazmussaif <nazmussaif@gmail.com>
2018-03-20 12:15:59 +0600

    from uiu: updated number

--- src/lab_2/number/number.java
+++ src/lab_2/number/number.java
@@ -2,7 +2,7 @@ package lab_2.number;
 
 public class number {
     public static void execute() {
-        System.out.println("number: ");
+        //System.out.println("number: ");
 
 //        State.setParameters(101);
 //        Node n = Node.BFS(new State(10));
