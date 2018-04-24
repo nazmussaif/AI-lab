b841b94f0a2f451cf0167712f52cdaad2cfc2633
nazmussaif <nazmussaif@gmail.com>
2018-04-24 13:31:01 +0600

    image classifiar

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
 
 public class Main {
-    public static void main(String[] args) {
-        n_queen.n_queen.execute();
+    public static void main(String[] args) throws Exception {
+        //n_queen.n_queen.execute();
+        knn_classifiar.knn_execute.execute();
     }
 }
---
+++ src/knn_classifiar/DigitImage.java
@@ -0,0 +1,13 @@
+package knn_classifiar;
+
+public class DigitImage {
+	int[] pixels;
+	int digit;
+
+	public DigitImage(int[] pixels, int digit) {
+		super();
+
+		this.pixels = pixels.clone();
+		this.digit = digit;
+	}
+}
